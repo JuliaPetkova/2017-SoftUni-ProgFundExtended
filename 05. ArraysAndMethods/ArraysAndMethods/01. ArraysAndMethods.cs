1dd397a3ee8e3b442768ac0551fa4923b1a3f6ba
JuliaPetkova <julia.petkova@gmail.com>
2017-06-17 20:52:47 +0300

    add

---
+++ 05. ArraysAndMethods/ArraysAndMethods/01. ArraysAndMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = HelloName();
+            Console.WriteLine($"Hello, {str}!");
+        }
+
+        static object HelloName()
+        {
+            string name;
+            name = Console.ReadLine();
+            return name;
+        }
+    }
+}
