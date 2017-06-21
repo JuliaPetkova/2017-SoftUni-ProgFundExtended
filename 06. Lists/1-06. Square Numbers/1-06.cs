a4211688d621ab782e8d1cfe106ccad9fadb2fc3
JuliaPetkova <julia.petkova@gmail.com>
2017-06-22 01:10:05 +0300

    add new task

---
+++ 06. Lists/1-06. Square Numbers/1-06.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_06.Square_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> input = Console.ReadLine().Split(' ').Select(double.Parse).OrderByDescending(a => a).ToList();
+
+            foreach (var item in input)
+            {
+                var temp = Math.Sqrt(item);
+                if (temp == (int)temp)
+                    Console.Write(item + " ");
+            }
+        }
+    }
+}
