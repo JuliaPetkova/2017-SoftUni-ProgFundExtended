81a39861244b322ec7f40f08d6b3101f1821aa6f
JuliaPetkova <julia.petkova@gmail.com>
2017-06-23 23:29:32 +0300

    add new tasks

---
+++ 06. Lists/2-02. Track Downloade/2-02.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_02.Track_Downloade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] blacklist = Console.ReadLine().Split(' ');
+
+            List<string> playlist = new List<string>();
+
+            string song = Console.ReadLine();
+
+            while (song != "end")
+            {
+                bool isContains = false;
+
+                for (int i = 0; i < blacklist.Length; i++)
+                {
+                    if (song.Contains(blacklist[i]))
+                    {
+                        isContains = true;
+                    }                   
+                }
+                if (!isContains)
+                {
+                    playlist.Add(song);
+                }
+                song = Console.ReadLine();
+            }
+            // playlist.Distinct();
+            playlist.Sort();
+            Console.WriteLine(string.Join("\r\n", playlist));
+
+        }
+    }
+}
---
+++ 06. Lists/2-03. Equal Sum After Extraction/2-03.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_03.Equal_Sum_After_Extraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstline = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> secondline = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int i = 0; i < firstline.Count; i++)
+            {
+                for (int j = 0; j < secondline.Count; j++)
+                {
+                    if (firstline[i] == secondline[j])
+                    {
+                        secondline.Remove(secondline[j]);
+                    }
+                }
+            }
+            var sum1 = firstline.Sum();
+            var sum2 = secondline.Sum();
+            var sum = Math.Abs(sum1 - sum2);
+
+            if (sum1 == sum2)
+            {
+                Console.WriteLine($"Yes. Sum: {sum1}");
+            }
+            else
+            {
+                Console.WriteLine( $"No. Diff: {sum}");
+            }
+        }
+    }
+}
