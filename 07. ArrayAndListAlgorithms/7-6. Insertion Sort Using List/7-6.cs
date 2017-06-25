e9d34dc6483b1c3f82d1726b5cd41f6b1db82732
JuliaPetkova <julia.petkova@gmail.com>
2017-06-25 23:59:39 +0300

    add new task

---
+++ 07. ArrayAndListAlgorithms/7-6. Insertion Sort Using List/7-6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_6.Insertion_Sort_Using_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int i = 1; i < arr.Count; i++)
+            {
+                int unsorted = arr[i];
+                int j;
+
+                for (j = i - 1; (j >= 0) && (arr[j] > unsorted); j--)
+                {
+                    arr[j + 1] = arr[j];
+                }
+
+                arr[j + 1] = unsorted;
+            }
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
---
+++ 07. ArrayAndListAlgorithms/7-7. Largest N Elements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_7.Largest_N_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int num = int.Parse(Console.ReadLine());
+
+            List<int> result = new List<int>();
+
+            for (int j = 1; j <= num; j++)
+            {
+                int maxvalue = int.MinValue;                                            
+
+                for (int i = 0; i < input.Count; i++)
+                {
+                    if (input[i] > maxvalue)
+                    {
+                        maxvalue = input[i];                        
+                    }
+                }
+                result.Add(maxvalue);
+
+                if (input.Contains(maxvalue))
+                {
+                    input.Remove(maxvalue);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
