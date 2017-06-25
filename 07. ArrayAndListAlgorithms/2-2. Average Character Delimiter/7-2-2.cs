28a6e829bda8c1ffc4185260838453d22ab5292a
JuliaPetkova <julia.petkova@gmail.com>
2017-06-26 01:32:14 +0300

    add tasks

---
+++ 07. ArrayAndListAlgorithms/2-2. Average Character Delimiter/7-2-2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_2.Average_Character_Delimiter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            int count = 0;
+
+            int charSum = 0;
+            string result = "";
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                foreach (var item in input[i])
+                {
+                    char number = item; // convert item (char) to an  integer 
+                    charSum = charSum + number;
+                    count++;
+                }
+            }
+            charSum = charSum / 10;
+
+            char sum = Convert.ToChar(charSum);
+            sum = Char.ToUpper(sum);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == input.Length - 1)
+                {
+                    result = result + input[i];
+                }
+                else
+                {
+                    result = result + input[i] + sum;
+                }
+            }
+            Console.WriteLine(result);
+
+            //Console.WriteLine($"count {count}, sum of chars in input {charSum}");
+        }
+    }
+}
