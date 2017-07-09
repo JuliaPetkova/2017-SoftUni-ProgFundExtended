5d4acf85b9433202e2fd6bbdf89e56c439dfabbb
JuliaPetkova <julia.petkova@gmail.com>
2017-07-10 01:41:53 +0300

    add

---
+++ 8. Dictionaries/2-5. User Logins/2-5. User Logins.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_5.User_Logins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, string> login = new Dictionary<string, string>();
+            int cnt = 0;
+
+            while (input[0] != "login")
+            {
+                if (!login.ContainsKey(input[0]))
+                {
+                    login.Add(input[0], input[1]);
+                }
+                else
+                {
+                    login[input[0]] = input[1];
+                }
+
+                input = Console.ReadLine().Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            input = Console.ReadLine().Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "end")
+            {
+
+                if (login.ContainsKey(input[0]) && login.ContainsValue(input[1]))
+                {
+                    Console.WriteLine($"{input[0]}: logged in successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"{input[0]}: logged in failed");
+                    cnt++;
+                }
+
+                input = Console.ReadLine().Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+            }
+
+            Console.WriteLine($"unsuccessful login attempts: {cnt}");
+        }
+    }
+}
---
+++ 8. Dictionaries/2-6. Filter Base/2-6. Filter Base.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_6.Filter_Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> age = new Dictionary<string, int>();
+            Dictionary<string, double> salary = new Dictionary<string, double>();
+            Dictionary<string, string> position = new Dictionary<string, string>();
+
+            while (input[0] != "filter")
+            {
+                if (true)
+                {
+
+                }
+                input = Console.ReadLine().Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            input = Console.ReadLine().Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+            if (input[0] == "Position")
+            {
+
+            }
+        }
+    }
+}
