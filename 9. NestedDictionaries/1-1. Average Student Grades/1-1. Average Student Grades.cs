5c93129aae61444fc90cb812272e3ec0c7acad32
JuliaPetkova <julia.petkova@gmail.com>
2017-07-11 23:15:36 +0300

    add

--- 9. NestedDictionaries/1-1. Average Student Grades/1-1. Average Student Grades.cs
+++ 9. NestedDictionaries/1-1. Average Student Grades/1-1. Average Student Grades.cs
@@ -11,10 +11,39 @@ namespace _1_1.Average_Student_Grades
     {
         static void Main(string[] args)
         {
-            Dictionary<string, List<double>> bbb =  new Dictionary<string, List<double>>();
-            
-            int numElements = int.Parse(Console.ReadLine());
-            c
+            var grades = new Dictionary<string, List<double>>();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                double grade = double.Parse(input[1]);
+
+                if (!grades.ContainsKey(name))
+                {
+                    grades[name] = new List<double>();
+                }
+                grades[name].Add(grade);
+            }
+
+            foreach (var item in grades)
+            {
+                var name = item.Key;
+                var grade = item.Value;
+
+                double average = grade.Average();
+
+                Console.Write($"{name} -> ");
+
+                foreach (var grd in grade)
+                {
+                    Console.Write($"{grd:F2} ");
+                }
+                Console.WriteLine($"(avg: {average:F2})");
+                
+            }
         }
     }
 }
---
+++ 9. NestedDictionaries/1-2. Cities by Continent and Country/1-2. Cities by Continent and Country.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2.Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var catalogue = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                string input = Console.ReadLine();
+                string[] tokens = input.Split();
+
+                string continent = tokens[0];
+                string country = tokens[1];
+                string city = tokens[2];
+
+                if (!catalogue.ContainsKey(continent))
+                {
+                    catalogue[continent] = new Dictionary<string, List<string>>();
+                }
+                if (!catalogue[continent].ContainsKey(country))
+                {
+                    catalogue[continent][country] = new List<string>();
+                }
+                catalogue[continent][country].Add(city);
+            }
+            foreach (KeyValuePair<string, Dictionary<string, List<string>>> data in catalogue)
+            {
+                var continent = data.Key;
+                var countries = data.Value;
+                Console.WriteLine($"{continent}:");
+
+                foreach (var item in countries)
+                {
+                    var country = item.Key;
+                    var cities = item.Value;
+
+                    Console.WriteLine(" {0} -> {1}", country, string.Join(", ", cities));
+                }
+            }
+        }
+    }
+}
+
